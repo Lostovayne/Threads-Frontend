f24f50ae2457e1d6c9de67da2c07b151a009d46a
Lostovayne <moises.barrera.dev@gmail.com>
2024-11-01 19:17:57 -0300

    agrega HeaderComponent

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
+import HeaderComponent from "@/components/common/header-mobile";
+
 export default function Home() {
   return (
     <>
+      <HeaderComponent />
       <h1>Home</h1>
     </>
   );
---
+++ components/common/header-mobile.tsx
@@ -0,0 +1,32 @@
+import { Separator } from "@/components/ui/separator";
+import { FC, ReactElement } from "react";
+
+import ButtonUiComponent from "./button-ui";
+import ThreadsComponent from "./icons/threads-icon";
+
+interface HeaderComponentProps {
+  // Prop types here
+}
+
+const HeaderComponent: FC<HeaderComponentProps> = ({}): ReactElement => {
+  return (
+    <>
+      <header className="h-22 flex flex-col items-center justify-between gap-2 p-2.5">
+        <div className="size-8">
+          <ThreadsComponent />
+        </div>
+        <div className="flex w-full items-center gap-2">
+          <ButtonUiComponent variant={"outline"}>
+            Iniciar sesión
+          </ButtonUiComponent>
+          <ButtonUiComponent className="bg-black" variant={"default"}>
+            Abrir app
+          </ButtonUiComponent>
+        </div>
+      </header>
+      <Separator className="my-1 w-full" />
+    </>
+  );
+};
+
+export default HeaderComponent;
