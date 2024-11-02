e34f413b553de0552177b1f83fec9767deeeb852
Ismytv <ismytv@gmail.com>
2024-11-02 13:36:21 -0500

    feat(post):en Format.SplitSentenceByHasTag agrega captura de saltos de linea

--- lib/format.ts
+++ lib/format.ts
@@ -1,7 +1,8 @@
 export class Format {
   static splitSentenceByHashTag(sentence: string) {
     // Expresión regular para identificar palabras que comienzan con @ o #
-    const regex = /(@\w+|#\w+)|([^@\s#]+|[.,!?])/g;
+    // y también capturar saltos de línea (\n)
+    const regex = /(@\w+|#\w+)|([^@\s#]+|[.,!?])|(\n)/g;
     const matches = sentence.match(regex);
 
     return matches || [];
