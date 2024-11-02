1dcd3faf38f5465904e89f6458025f5a1b48d924
Ismytv <ismytv@gmail.com>
2024-11-02 13:04:43 -0500

    feat(post):agrega llamada a componente PostMain desde RootPage para pruebas,

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,33 @@
+import { PostMain } from "@/components/features/posts/post-main";
+import { POST_DATA } from "@/data/post.data";
+
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
+
+      <PostList />
     </>
   );
+}
+
+const PostList = () => {
+  return (
+    <div className="flex flex-col mt-2 gap-4 sm:gap-0 px-1">
+      {
+        POST_DATA.map((post) => (
+          <PostMain
+            key={post.id}
+            id={post.id}
+            urlProfile={post.urlProfile}
+            name={post.name}
+            date={post.date}
+            description={post.description}
+            media={post.media}
+            reactions={post.reactions}
+          />
+        ))
+      }
+    </div>
+  )
 }
