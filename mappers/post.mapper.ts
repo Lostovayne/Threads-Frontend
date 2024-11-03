aafc1a77e9e195e92e225323c0bc15aaa6428e98
Ismytv <ismytv@gmail.com>
2024-11-03 10:42:19 -0500

    refactor(post):disgrega PostView; elimina Post

--- mappers/post.mapper.ts
+++ mappers/post.mapper.ts
@@ -1,15 +1,2 @@
-import { PostView } from "@/interfaces/post/post-view.interface";
-import { Post } from "@/interfaces/post/post.interface";
-
 export class PostMapper {
-    static toPostView(post: Post): PostView {
-        return {
-            urlProfile: post.urlProfile,
-            name: post.name,
-            date: post.date,
-            description: post.description,
-            media: post.media,
-            reactions: post.reactions
-        }
-    }
 }
