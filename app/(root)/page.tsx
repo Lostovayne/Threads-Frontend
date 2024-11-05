d5dffba96efea2a4ebe0c754320e9426ca09dafa
Deus lo Vult <moises.barrera.dev@gmail.com>
2024-11-05 00:09:55 -0300

    feat: :zap: add navbar element -

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import HeaderComponent from "@/components/common/header-mobile";
+import { NavDesktop } from "@/components/common/nav/nav-desktop";
+import { NavMobile } from "@/components/common/nav/nav-mobile";
 import { Post } from "@/components/features/posts/post";
 import { Separator } from "@/components/ui/separator";
 import { POST_DATA } from "@/data/post.data";
@@ -7,6 +9,8 @@ export default function Home() {
   return (
     <>
       <HeaderComponent />
+      <NavDesktop />
+      <NavMobile />
       <section className="mx-auto mt-10 max-w-[650px] rounded-3xl border border-zinc-300/70 bg-white shadow-xl max-md:pr-3">
         <PostList />
       </section>
