79f1999c545267f8f092849cd644c953f28c4767
ismytv <ismytv@gmail.com>
2024-11-08 18:19:35 -0500

    chore: instalar todas las dependencias (bun install)
     feat(auth): agregar componente AuthRegistrationModal; chore: agregar shadcn/dialog

---
+++ components/features/auth/auth-registration-modal.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { FC, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Camera } from "lucide-react";
+import { ChevronRight } from 'lucide-react';
+import MessageIcon from "../posts/icons/message";
+
+interface AuthRegistrationModalProps {
+  open: boolean
+  setIsOpen: () => void
+}
+
+export const AuthRegistrationModal:FC<AuthRegistrationModalProps> = ({open, setIsOpen}) => {
+
+  return (
+    <Dialog open={open} onOpenChange={setIsOpen}>
+      {/* <DialogTrigger asChild>
+        <Button variant="outline">Share</Button>
+      </DialogTrigger> */}
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle className="flex w-full flex-col items-center gap-[1rem]">
+            <div className="text-red-500 [&_svg]:size-[3.25rem]">
+              <MessageIcon />
+            </div>
+            <span className="text-3xl font-extrabold">
+              Regístrate para indicar que te gusta
+            </span>
+          </DialogTitle>
+          <DialogDescription>
+            Únete a Threads para interactuar con publicaciones e indicar que te
+            gustan.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter className="mt-3">
+          <DialogClose asChild>
+            <Button
+              type="button"
+              variant="ghost"
+              onClick={() => console.log("hola")}
+              className="flex gap-5 p-7 justify-between"
+            >
+              <Camera style={{height: 36, width: 36 }} />
+              <span className="text-md font-bold">Continuar con Instagram!</span>
+              <ChevronRight  />
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- components/features/posts/post-reactions.tsx
+++ components/features/posts/post-reactions.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { FC, ReactElement } from "react";
 
 import ReactionButton from "./components/reaction-button";
@@ -7,16 +9,19 @@ import RepostIcon from "./icons/repost";
 import SendIcon from "./icons/send";
 import { PostViewReactions } from "@/interfaces/post/post-view.interface";
 import { Format } from "@/lib/format";
+import { AuthRegistrationModal } from "../auth/auth-registration-modal";
 
 export const PostReactions: FC<PostViewReactions> = (reactions): ReactElement => {
   const { likes, comments, reposts, shares } = reactions;
   
   return (
     <div className="pt-3 pb-2 flex w-1/3 items-center gap-1 ">
-      <ReactionButton>
+      <ReactionButton onClick={() => 
+        console.log("Me Gusta")
+      }>
         <HeartIcon /> {Format.numberReactions(likes)}
       </ReactionButton>
-      <ReactionButton>
+      <ReactionButton onClick={() => console.log("Comentar")}>
         <MessageIcon /> {Format.numberReactions(comments)}
       </ReactionButton>
       <ReactionButton>
